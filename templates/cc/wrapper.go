8c498d5b19a0c86fba425b4338120ba9c0029dc0
akonradi <akonradi@google.com>
2017-11-30 17:48:18 -0500

    Translate wrapped type validation to C++ (#33)

--- templates/cc/wrapper.go
+++ templates/cc/wrapper.go
@@ -3,7 +3,8 @@ package tpl
 const wrapperTpl = `
 	{{ $f := .Field }}{{ $r := .Rules }}
 
-	if wrapper := {{ accessor . }}; wrapper != nil {
-		{{ render (unwrap . "wrapper") }}
+	if ({{ hasAccessor . }}) {
+		const auto wrapped = {{ accessor . }};
+		{{ render (unwrap . "wrapped") }}
 	}
 `
