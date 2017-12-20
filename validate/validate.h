077902da2b7e4edee0f146f445c6879a3eaea928
Alex Konradi <akonradi@google.com>
2017-12-20 18:33:54 -0500

    Use better names for validate.h string functions

--- validate/validate.h
+++ validate/validate.h
@@ -12,17 +12,20 @@ using std::string;
 namespace protobuf = google::protobuf;
 namespace protobuf_wkt = google::protobuf;
 
-static inline bool IsPrefix(const string& needle, const string& haystack) {
-  return haystack.compare(0, needle.size(), needle) == 0;
+static inline bool IsPrefix(const string& maybe_prefix,
+                            const string& search_in) {
+  return search_in.compare(0, maybe_prefix.size(), maybe_prefix) == 0;
 }
 
-static inline bool IsSuffix(const string& needle, const string& haystack) {
-  return (needle.size() <= haystack.size()) &&
-    (haystack.compare(haystack.size() - needle.size(), needle.size(), needle) == 0);
+static inline bool IsSuffix(const string& maybe_suffix,
+                            const string& search_in) {
+  return (maybe_suffix.size() <= search_in.size()) &&
+    (search_in.compare(search_in.size() - maybe_suffix.size(),
+                       maybe_suffix.size(), maybe_suffix) == 0);
 }
 
-static inline bool Contains(const string& haystack, const string& needle) {
-  return haystack.find(needle) != string::npos;
+static inline bool Contains(const string& search_in, const string& to_find) {
+  return search_in.find(to_find) != string::npos;
 }
 
 } // namespace pgv
