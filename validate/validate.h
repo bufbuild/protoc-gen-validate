2beef31247b1bcdf00b74999a1fdd65f00ab94c3
Andrew Keesler <akeesler@pivotal.io>
2018-07-30 17:29:04 -0400

    Use static registration for cross-package validation
    When compiling with MSVC, there is not an equivalent to__attribute__(weak).
    In order for cross-package validation to work with MSVC, use static
    registration instead
    
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- validate/validate.h
+++ validate/validate.h
@@ -3,6 +3,7 @@
 
 #include <stdexcept>
 #include <string>
+#include <typeinfo>
 
 #include <google/protobuf/message.h>
 #include <google/protobuf/util/time_util.h>
@@ -14,30 +15,58 @@ namespace protobuf = google::protobuf;
 namespace protobuf_wkt = google::protobuf;
 
 class UnimplementedException : public std::runtime_error {
- public:
+public:
   UnimplementedException() : std::runtime_error("not yet implemented") {}
   // Thrown by C++ validation code that is not yet implemented.
 };
 
 using ValidationMsg = std::string;
 
-static inline std::string String(const ValidationMsg& msg) {
+class BaseValidator {
+protected:
+  static std::map<size_t, BaseValidator*> validators;
+};
+
+std::map<size_t, BaseValidator*> __attribute__((weak)) BaseValidator::validators;
+
+template <typename T>
+class Validator : public BaseValidator {
+public:
+  Validator(std::function<bool(const T&, ValidationMsg*)> check) : check_(check)
+  {
+    validators[typeid(T).hash_code()] = this;
+  }
+
+  static bool CheckMessage(const T& m, ValidationMsg* err)
+  {
+    auto val = static_cast<Validator<T>*>(validators[typeid(T).hash_code()]);
+    if (val) {
+      return val->check_(m, err);
+    }
+    return true;
+  }
+
+private:
+  std::function<bool(const T&, ValidationMsg*)> check_;
+};
+
+static inline std::string String(const ValidationMsg& msg)
+{
   return std::string(msg);
 }
 
-static inline bool IsPrefix(const string& maybe_prefix,
-                            const string& search_in) {
+static inline bool IsPrefix(const string& maybe_prefix, const string& search_in)
+{
   return search_in.compare(0, maybe_prefix.size(), maybe_prefix) == 0;
 }
 
-static inline bool IsSuffix(const string& maybe_suffix,
-                            const string& search_in) {
-  return maybe_suffix.size() <= search_in.size() &&
-    search_in.compare(search_in.size() - maybe_suffix.size(),
-                       maybe_suffix.size(), maybe_suffix) == 0;
+static inline bool IsSuffix(const string& maybe_suffix, const string& search_in)
+{
+  return maybe_suffix.size() <= search_in.size() && search_in.compare(search_in.size() - maybe_suffix.size(), maybe_suffix.size(), maybe_suffix) == 0;
 }
 
-static inline bool Contains(const string& search_in, const string& to_find) {
+static inline bool Contains(const string& search_in, const string& to_find)
+{
   return search_in.find(to_find) != string::npos;
 }
 
