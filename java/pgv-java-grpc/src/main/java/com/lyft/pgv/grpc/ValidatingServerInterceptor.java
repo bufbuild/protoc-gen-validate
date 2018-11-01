aaaf07f39961cef50b715ca62925e8fed76d4967
Ryan Michela <rmichela@salesforce.com>
2018-10-31 23:24:19 -0700

    Don't double half-close grpc server calls

--- java/pgv-java-grpc/src/main/java/com/lyft/pgv/grpc/ValidatingServerInterceptor.java
+++ java/pgv-java-grpc/src/main/java/com/lyft/pgv/grpc/ValidatingServerInterceptor.java
@@ -4,6 +4,8 @@ import com.lyft.pgv.ValidationException;
 import com.lyft.pgv.ValidatorIndex;
 import io.grpc.*;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ValidatingServerInterceptor implements ServerInterceptor {
     private final ValidatorIndex index;
 
@@ -14,6 +16,11 @@ public class ValidatingServerInterceptor implements ServerInterceptor {
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
         return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(next.startCall(call, headers)) {
+
+            // Implementations are free to block for extended periods of time. Implementations are not
+            // required to be thread-safe.
+            private boolean aborted = false;
+
             @Override
             public void onMessage(ReqT message) {
                 try {
@@ -21,7 +28,15 @@ public class ValidatingServerInterceptor implements ServerInterceptor {
                     super.onMessage(message);
                 } catch (ValidationException ex) {
                     Status status = Status.INVALID_ARGUMENT.withDescription(ex.getMessage());
+                    aborted = true;
                     call.close(status, new Metadata());
+                }
+            }
+
+            @Override
+            public void onHalfClose() {
+                if (!aborted) {
+                    super.onHalfClose();
                 }
             }
         };
