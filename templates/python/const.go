567a3a6a28e3610f50f1f148711246cb409b5920
guavacole <henryquan.tum@gmail.com>
2019-03-15 13:04:34 -0700

    wip

--- templates/python/const.go
+++ templates/python/const.go
@@ -3,6 +3,6 @@ package python
 const constTpl = `{{ $r := .Rules }}
 	{{ if $r.Const }}
 		if {{ accessor . }} != {{ lit $r.GetConst }} :
-			{{err . "Cannot validate" }}
+			return False
 	{{ end }}
 `
