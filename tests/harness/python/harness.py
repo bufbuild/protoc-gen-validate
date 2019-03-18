4ea57d90a8fcf62dfbd50ade446e5352be13f3cf
guavacole <henryquan.tum@gmail.com>
2019-03-18 11:34:52 -0700

    wip

--- tests/harness/python/harness.py
+++ tests/harness/python/harness.py
@@ -1,7 +1,7 @@
 import sys
 
 from harness_pb2 import TestCase, TestResult
-from tests.harness.cases import bool_pb_validate
+import bool_pb_validate
 
 if __name__ == "__main__":
     lines = sys.stdin.readlines()
