cc060297a3e43f6daad597fa1f537de0540d7311
Ryan Michela <rmichela@salesforce.com>
2019-08-29 15:14:28 -0400

    Partial diff checking
    Signed-off-by: Ryan Michela <rmichela@salesforce.com>

--- protolock-pgv/plugin.go
+++ protolock-pgv/plugin.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
+	"fmt"
 
 	"github.com/nilslice/protolock"
 	"github.com/nilslice/protolock/extend"
@@ -11,16 +10,7 @@ import (
 func main() {
 	plugin := extend.NewPlugin("pgv")
 	plugin.Init(func(data *extend.Data) *extend.Data {
-		f, _ := os.Create("debug.txt")
-		defer f.Close()
-
-		data.PluginWarnings = append(data.PluginWarnings, compareDefinitions(data.Current, data.Updated, f)...)
-
-		f.WriteString("======================\n")
-
-		b, _ := json.MarshalIndent(data, "", "  ")
-		f.Write(b)
-
+		data.PluginWarnings = append(data.PluginWarnings, compareDefinitions(data.Current, data.Updated)...)
 		return data
 	})
 }
@@ -28,7 +18,7 @@ func main() {
 // Only compare definitions that exist in both current and updated. A new definition in updated can't be wrong
 // because it is new. A deleted definition in updated is already flagged by base protolock as a breaking change.
 
-func compareDefinitions(current protolock.Protolock, updated protolock.Protolock, f *os.File) []protolock.Warning {
+func compareDefinitions(current protolock.Protolock, updated protolock.Protolock) []protolock.Warning {
 	warnings := []protolock.Warning{}
 
 	// Assume definitions are sorted by Definition.Filepath
@@ -36,8 +26,7 @@ func compareDefinitions(current protolock.Protolock, updated protolock.Protolock
 		cur, upd := current.Definitions[c], updated.Definitions[u]
 
 		if cur.Filepath == upd.Filepath {
-			f.WriteString("Comparing " + cur.Filepath.String() + "\n")
-			warnings = append(warnings, compareMessages(cur.Def.Messages, upd.Def.Messages, upd.Filepath, f)...)
+			warnings = append(warnings, compareMessages(cur.Def.Messages, upd.Def.Messages, upd.Filepath)...)
 			c++
 			u++
 		} else if cur.Filepath < upd.Filepath {
@@ -50,7 +39,7 @@ func compareDefinitions(current protolock.Protolock, updated protolock.Protolock
 	return warnings
 }
 
-func compareMessages(current []protolock.Message, updated []protolock.Message, filepath protolock.Protopath, f *os.File) []protolock.Warning {
+func compareMessages(current []protolock.Message, updated []protolock.Message, filepath protolock.Protopath) []protolock.Warning {
 	warnings := []protolock.Warning{}
 
 	// Assume messages are sorted by Message.Name
@@ -58,8 +47,8 @@ func compareMessages(current []protolock.Message, updated []protolock.Message, f
 		cur, upd := current[c], updated[u]
 
 		if cur.Name == upd.Name {
-			f.WriteString("  Comparing " + cur.Name + "\n")
-			warnings = append(warnings, compareFields(cur.Fields, upd.Fields, filepath, f)...)
+			warnings = append(warnings, compareFields(fmt.Sprintf("%q", cur.Name), cur.Fields, upd.Fields, filepath)...)
+			// warnings = append(warnings, compareFields("\""+cur.Name+"\"", cur.Fields, upd.Fields, filepath)...)
 			c++
 			u++
 		} else if cur.Name < upd.Name {
@@ -72,7 +61,7 @@ func compareMessages(current []protolock.Message, updated []protolock.Message, f
 	return warnings
 }
 
-func compareFields(current []protolock.Field, updated []protolock.Field, filepath protolock.Protopath, f *os.File) []protolock.Warning {
+func compareFields(where string, current []protolock.Field, updated []protolock.Field, filepath protolock.Protopath) []protolock.Warning {
 	warnings := []protolock.Warning{}
 
 	// Assume messages are sorted by Message.Name
@@ -80,8 +69,8 @@ func compareFields(current []protolock.Field, updated []protolock.Field, filepat
 		cur, upd := current[c], updated[u]
 
 		if cur.Name == upd.Name {
-			f.WriteString("    Comparing " + cur.Name + "\n")
-			warnings = append(warnings, compareOptions(cur.Options, upd.Options, filepath, f)...)
+			warnings = append(warnings, compareOptions(fmt.Sprintf("%s field: %q", where, cur.Name), flatOptions(cur.Options), flatOptions(upd.Options), filepath)...)
+			// warnings = append(warnings, compareOptions(where+" field: \""+cur.Name+"\"", flatOptions(cur.Options), flatOptions(upd.Options), filepath)...)
 			c++
 			u++
 		} else if cur.Name < upd.Name {
@@ -94,19 +83,52 @@ func compareFields(current []protolock.Field, updated []protolock.Field, filepat
 	return warnings
 }
 
-func compareOptions(current []protolock.Option, updated []protolock.Option, filepath protolock.Protopath, f *os.File) []protolock.Warning {
+// Flatten a []protolock.Options, decomposing aggregated options into multiple separate Options
+func flatOptions(optionsWithAggregates []protolock.Option) []protolock.Option {
+	options := []protolock.Option{}
+
+	for _, opt := range optionsWithAggregates {
+		if opt.Aggregated == nil {
+			options = append(options, opt)
+		} else {
+			for _, agg := range opt.Aggregated {
+				options = append(options, protolock.Option{
+					Name:  opt.Name + "." + agg.Name,
+					Value: agg.Value,
+				})
+			}
+		}
+	}
+
+	return options
+}
+
+func compareOptions(where string, current []protolock.Option, updated []protolock.Option, filepath protolock.Protopath) []protolock.Warning {
 	warnings := []protolock.Warning{}
+
+	nullTerm := protolock.Option{
+		Name: "NULL-TERMINATOR-NULL-TERMINATOR",
+	}
+
+	// Add "null terminators" to ensure empty options lists don't prematurely crash out of the loop
+	current = append(current, nullTerm)
+	updated = append(updated, nullTerm)
 
 	// Assume messages are sorted by Message.Name
 	for c, u := 0, 0; c < len(current) && u < len(updated); {
 		cur, upd := current[c], updated[u]
 
 		if cur.Name == upd.Name {
-			f.WriteString("      Comparing " + cur.Name + "\n")
-			warnings = append(warnings, compareOption(cur, upd, filepath, f)...)
+			warnings = append(warnings, compareOption(where, cur, upd, filepath)...)
 			c++
 			u++
 		} else if cur.Name < upd.Name {
+			warnings = append(warnings, protolock.Warning{
+				Filepath: filepath,
+				Message:  fmt.Sprintf("%s constraint: %q has been removed", where, cur.Name),
+				// Message:  where + " constraint: \"" + cur.Name + "\" has been removed",
+				RuleName: "PGV",
+			})
 			c++
 		} else {
 			u++
@@ -116,14 +138,14 @@ func compareOptions(current []protolock.Option, updated []protolock.Option, file
 	return warnings
 }
 
-func compareOption(current protolock.Option, updated protolock.Option, filepath protolock.Protopath, f *os.File) []protolock.Warning {
+func compareOption(where string, current protolock.Option, updated protolock.Option, filepath protolock.Protopath) []protolock.Warning {
 	warnings := []protolock.Warning{}
 
-	f.WriteString("        " + current.Value + " : " + updated.Value + "\n")
 	if current.Value != updated.Value {
 		warnings = append(warnings, protolock.Warning{
 			Filepath: filepath,
-			Message:  current.Value + " != " + updated.Value,
+			Message:  fmt.Sprintf("%s constraint: %q has changed from %s to %s", where, current.Name, current.Value, updated.Value),
+			// Message:  where + " constraint: \"" + current.Name + "\" has changed from " + current.Value + " to " + updated.Value,
 			RuleName: "PGV",
 		})
 	}
