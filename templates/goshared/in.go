d41b7febfe52bfe312d89babd6ec0c4f8b9b317b
Cody A. Ray <cody@confluent.io>
2020-10-14 14:42:22 -0500

    typo template
    Signed-off-by: Cody A. Ray <cody@confluent.io>

--- templates/goshared/in.go
+++ templates/goshared/in.go
@@ -3,7 +3,7 @@ package goshared
 const inTpl = `{{ $f := .Field }}{{ $r := .Rules }}
 	{{ if $r.In }}
 		if _, ok := {{ lookup $f "InLookup" }}[{{ accessor . }}]; !ok {
-			return {{ err . (t "<prefix>.in" value must be in list {{$1}}" $r.In) }}
+			return {{ err . (t "<prefix>.in" "value must be in list {{$1}}" $r.In) }}
 		}
 	{{ else if $r.NotIn }}
 		if _, ok := {{ lookup $f "NotInLookup" }}[{{ accessor . }}]; ok {
