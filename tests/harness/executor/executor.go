aeaac80e6f5290552a14567d9063d79f5cec7f9b
Alex Konradi <akonradi@google.com>
2021-03-11 16:46:44 -0500

    Support Bazel test sharding
    This makes for better use of development resources. Since the test cases
    are completely independent, they can be split among multiple invocations of
    the test executor without any coordination.
    
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- tests/harness/executor/executor.go
+++ tests/harness/executor/executor.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"log"
+	"math"
 	"os"
 	"runtime"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -23,9 +25,10 @@ func main() {
 	externalHarnessFlag := flag.String("external_harness", "", "Path to a binary to be executed as an external test harness")
 	flag.Parse()
 
+	test_cases := shardTestCases(TestCases)
 	start := time.Now()
 	harnesses := Harnesses(*goFlag, *ccFlag, *javaFlag, *pythonFlag, *externalHarnessFlag)
-	successes, failures, skips := run(*parallelism, harnesses)
+	successes, failures, skips := run(*parallelism, harnesses, test_cases)
 
 	log.Printf("Successes: %d | Failures: %d | Skips: %d (%v)",
 		successes, failures, skips, time.Since(start))
@@ -35,7 +38,39 @@ func main() {
 	}
 }
 
-func run(parallelism int, harnesses []Harness) (successes, failures, skips uint64) {
+func shardTestCases(test_cases []TestCase) []TestCase {
+	// Support Bazel test sharding by slicing up the list of test cases if requested.
+	shard_count, err := strconv.Atoi(os.Getenv("TEST_TOTAL_SHARDS"))
+	if err != nil {
+		return test_cases
+	}
+
+	shard_index, err := strconv.Atoi(os.Getenv("TEST_SHARD_INDEX"))
+	if err != nil {
+		return test_cases
+	}
+
+	// Bazel expects that the test will create or modify the file with the provided name to show that it supports sharding.
+	status_file := os.Getenv("TEST_SHARD_STATUS_FILE")
+	if status_file == "" {
+		return test_cases
+	}
+
+	if file, err := os.Create(status_file); err != nil {
+		return test_cases
+	} else {
+		file.Close()
+	}
+	shard_length := int(math.Ceil(float64(len(test_cases)) / float64(shard_count)))
+	shard_start := shard_index * shard_length
+	shard_end := (shard_index + 1) * shard_length
+	if shard_end >= len(test_cases) {
+		shard_end = len(test_cases)
+	}
+	return test_cases[shard_start:shard_end]
+}
+
+func run(parallelism int, harnesses []Harness, test_cases []TestCase) (successes, failures, skips uint64) {
 	wg := new(sync.WaitGroup)
 	if parallelism <= 0 {
 		panic("Parallelism must be > 0")
@@ -66,7 +101,7 @@ func run(parallelism int, harnesses []Harness) (successes, failures, skips uint6
 		close(done)
 	}()
 
-	for _, test := range TestCases {
+	for _, test := range test_cases {
 		in <- test
 	}
 	close(in)
