4e31f9a0bc22577e5b44f6308e5acd849c0ddbac
Karen <8099322@qq.com>
2021-10-27 20:35:08 +0800

    Add support proto3 optional
    Signed-off-by: Karen <8099322@qq.com>

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"github.com/envoyproxy/protoc-gen-validate/module"
 	"github.com/lyft/protoc-gen-star"
 	"github.com/lyft/protoc-gen-star/lang/go"
-	"github.com/envoyproxy/protoc-gen-validate/module"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
+	var supportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 	pgs.
-		Init(pgs.DebugEnv("DEBUG_PGV")).
+		Init(pgs.DebugEnv("DEBUG_PGV"), pgs.SupportedFeatures(&supportedFeatures)).
 		RegisterModule(module.Validator()).
 		RegisterPostProcessor(pgsgo.GoFmt()).
 		Render()
