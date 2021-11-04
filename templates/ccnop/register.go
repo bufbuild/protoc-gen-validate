15d0f849d850f7c4b80e7b50b6a4920092298aa2
Martin Conte Mac Donell <reflejo@gmail.com>
2021-11-03 23:43:14 -0700

    introduce a NOP template for cc
    Signed-off-by: Martin Conte Mac Donell <reflejo@gmail.com>

---
+++ templates/ccnop/register.go
@@ -0,0 +1,17 @@
+package ccnop
+
+import (
+	"text/template"
+
+	"github.com/envoyproxy/protoc-gen-validate/templates/cc"
+	pgs "github.com/lyft/protoc-gen-star"
+)
+
+func RegisterModule(tpl *template.Template, params pgs.Parameters) {
+	cc.RegisterModule(tpl, params)
+	template.Must(tpl.Parse(moduleFileTpl))
+}
+
+func RegisterHeader(tpl *template.Template, params pgs.Parameters) {
+	cc.RegisterHeader(tpl, params)
+}
