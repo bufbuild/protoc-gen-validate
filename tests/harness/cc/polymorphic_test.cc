b27d1105e868475e57221ed0c4f4a92a1f3fd32f
Matt Klein <mklein@lyft.com>
2022-03-02 03:56:10 +0000

    fix
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- tests/harness/cc/polymorphic_test.cc
+++ tests/harness/cc/polymorphic_test.cc
@@ -8,13 +8,13 @@ int main() {
   // This does not have an associated validator but should still pass.
   std::string err;
   if (!pgv::BaseValidator::AbstractCheckMessage(foo, &err)) {
-    EXIT_FAILURE;
+    return EXIT_FAILURE;
   }
 
   tests::harness::cases::BoolConstTrue bool_const_true;
   bool_const_true.set_val(false);
-  if (pgv::BaseValidator::AbstractCheckMessage(foo, &err)) {
-    EXIT_FAILURE;
+  if (pgv::BaseValidator::AbstractCheckMessage(bool_const_true, &err)) {
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
