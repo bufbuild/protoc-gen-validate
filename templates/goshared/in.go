a8a417199b43ac2e24bb79bc77d34a3d1b82366f
Elbek Khoshimjonov <elbek.khoshimjonov@atomicvest.com>
2022-10-28 00:33:02 +0500

    in/not_in show enum values instead of numbers

--- templates/goshared/in.go
+++ templates/goshared/in.go
@@ -3,13 +3,13 @@ package goshared
 const inTpl = `{{ $f := .Field }}{{ $r := .Rules }}
 	{{ if $r.In }}
 		if _, ok := {{ lookup $f "InLookup" }}[{{ accessor . }}]; !ok {
-			err := {{ err . "value must be in list " $r.In }}
+			err := {{ err . "value must be in list " (list $f $r.In) }}
 			if !all { return err }
 			errors = append(errors, err)
 		}
 	{{ else if $r.NotIn }}
 		if _, ok := {{ lookup $f "NotInLookup" }}[{{ accessor . }}]; ok {
-			err := {{ err . "value must not be in list " $r.NotIn }}
+			err := {{ err . "value must not be in list " (list $f $r.NotIn) }}
 			if !all { return err }
 			errors = append(errors, err)
 		}
