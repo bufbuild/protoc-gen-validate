3f75e69a6853ca82fd3baa132b6ffd669289e378
Philip K. Warren <pkwarren@users.noreply.github.com>
2022-11-07 14:24:08 -0600

    add support for multiple plugins (#717)
    Instead of requiring a 'lang' option to be specified, create separate
    binaries configured to output code for a given language.
    
    Co-authored-by: Elliot Jackson <13633636+elliotmjackson@users.noreply.github.com>

---
+++ cmd/protoc-gen-validate-go/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/envoyproxy/protoc-gen-validate/module"
+	pgs "github.com/lyft/protoc-gen-star"
+	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+func main() {
+	optional := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+	pgs.
+		Init(pgs.DebugEnv("DEBUG_PGV"), pgs.SupportedFeatures(&optional)).
+		RegisterModule(module.ValidatorForLanguage("go")).
+		RegisterPostProcessor(pgsgo.GoFmt()).
+		Render()
+}
