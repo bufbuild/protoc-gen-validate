0bd37737a937241b71ae65018279568b4daf8c0d
Dhi Aurrahman <dio@rockybars.com>
2022-12-21 06:12:40 +0700

    Proposal: Improve Makefile (#735)
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>
    Co-authored-by: Elliot Jackson <13633636+elliotmjackson@users.noreply.github.com>

---
+++ Tools.mk
@@ -0,0 +1,5 @@
+# Licensed under the Apache License, Version 2.0 (the "License")
+
+bazelisk@v      := github.com/bazelbuild/bazelisk@v1.15.0
+protoc@v        := github.com/protocolbuffers/protobuf@v21.9
+protoc-gen-go@v := google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
