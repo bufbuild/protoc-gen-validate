888f517d86687ac5ae72d82f502fb2a4dfef73c7
bufdev <bufdev-github@buf.build>
2023-01-23 13:20:06 -0500

    simplify

--- internal/internal.go
+++ internal/internal.go
@@ -4,7 +4,6 @@ package internal
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 )
 
 var version = "0.10.0-dev"
@@ -13,7 +12,7 @@ var version = "0.10.0-dev"
 // arg is "--version". If so, it prints the current version and exits.
 func CheckVersionFlag() {
 	if len(os.Args) == 2 && os.Args[1] == "--version" {
-		fmt.Fprintf(os.Stdout, "%v %v\n", filepath.Base(os.Args[0]), version)
+		fmt.Println(version)
 		os.Exit(0)
 	}
 }
