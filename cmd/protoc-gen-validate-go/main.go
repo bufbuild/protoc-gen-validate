e070bacd3a44bed45c6db41e3b74bbcb824a73b6
bufdev <bufdev-github@buf.build>
2023-01-23 13:16:23 -0500

    Add --version to all protoc plugins

--- cmd/protoc-gen-validate-go/main.go
+++ cmd/protoc-gen-validate-go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/envoyproxy/protoc-gen-validate/internal"
 	"github.com/envoyproxy/protoc-gen-validate/module"
 	pgs "github.com/lyft/protoc-gen-star"
 	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	internal.CheckVersionFlag()
 	optional := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 	pgs.
 		Init(pgs.DebugEnv("DEBUG_PGV"), pgs.SupportedFeatures(&optional)).
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"github.com/envoyproxy/protoc-gen-validate/internal"
 	"github.com/envoyproxy/protoc-gen-validate/module"
-	"github.com/lyft/protoc-gen-star"
-	"github.com/lyft/protoc-gen-star/lang/go"
+	pgs "github.com/lyft/protoc-gen-star"
+	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
+	internal.CheckVersionFlag()
 	optional := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 	pgs.
 		Init(pgs.DebugEnv("DEBUG_PGV"), pgs.SupportedFeatures(&optional)).
