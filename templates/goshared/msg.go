c220710fc5a66140e40b7d36d4952548253883a1
apocelipes <seve3r@outlook.com>
2025-01-03 03:45:09 +0800

    Prealloc msgs in multierr.Error (#1209)
    "msgs"'s length is known, preallocate it can save memories.
    
    Co-authored-by: Chris Roche <github@rodaine.com>

--- templates/goshared/msg.go
+++ templates/goshared/msg.go
@@ -95,7 +95,7 @@ type {{ multierrname . }} []error
 
 // Error returns a concatenation of all the error messages it wraps.
 func (m {{ multierrname . }}) Error() string {
-	var msgs []string
+	msgs := make([]string, 0, len(m))
 	for _, err := range m {
 		msgs = append(msgs, err.Error())
 	}
